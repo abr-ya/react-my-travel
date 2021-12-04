3407ad74e29230c347ea56cc3d023a3073ff05a7
abr-ya <gamer05@list.ru>
2021-12-04 16:16:03 +0300

    feat: Add filters component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Button } from "@abr-ya/lib-travel-kit-demo";
 // import classNames from "classnames";
-// import { useWindowWidth } from "@react-hook/window-size";
+import { useWindowWidth } from "@react-hook/window-size";
 // import useDarkMode from "./hooks/useDarkMode";
+import { Filters } from "./components/";
 
 const App = () => {
   const btnClickHandler = () => console.log("клик!");
   // const { darkMode } = useDarkMode({ forceDark: false });
-  // const windowWidth = useWindowWidth();
+  const windowWidth = useWindowWidth();
+  console.log("windowWidth", windowWidth);
 
   return (
     <div>
@@ -15,6 +17,7 @@ const App = () => {
       <Button variant="primary" onClick={btnClickHandler}>
         Я кнопко!
       </Button>
+      <Filters />
     </div>
   );
 };
